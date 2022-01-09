7bc9cd60b6fcacfac664add11483e46951919d97
theneverends <wangricky3@gmail.com>
2022-01-09 04:17:10 -0500

    upload

---
+++ mywebsite/src/Directories/Home/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+export default function Footer() {
+    return (
+        <div className='footer-directory'>
+            <div className='footer-parent'>
+                <img src={require ('../../../assets/Home/shape-bg.png').default} alt='no internet connection'>
+                </img>
+            </div>
+        </div>
+    )
+}
